a7a5e91aca885b67fd6391b0b23ea87f8f16705b
Victor-Atanacio <124050394@upq.edu.mx>
2025-09-10 14:36:40 -0600

    Se creo un array y un objeto

--- JSParaPrincipiantes/java.js
+++ JSParaPrincipiantes/java.js
@@ -1,9 +1,6 @@
 document.writeln("<h1>Hola mundo</h1>"); 
-
 console.log("Hola");
-
 console.error("hay un error");
-
 //Tipos de datos en js
 
 // Cadenas
@@ -25,3 +22,12 @@ false // Falso
 ["Victor","Juan","Jairo"];
 [1,2,3];
 [true,false,true,false];
+
+//Objecto
+ 
+console.log({
+   "username": 'ryan',
+    "score": 70.4,
+    "hours":14,
+    "Proffesional": true
+});
