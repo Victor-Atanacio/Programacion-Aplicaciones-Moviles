7912e32f0243798305dad0d7787bfff53251b29d
Victor-Atanacio <124050394@upq.edu.mx>
2025-09-10 14:23:16 -0600

    se creo un hola mundo y los tipos de datos

--- JSParaPrincipiantes/java.js
+++ JSParaPrincipiantes/java.js
@@ -1,5 +1,27 @@
-document.writeln("<h1>Hola mundo</h1>"); // mostsrar 
+document.writeln("<h1>Hola mundo</h1>"); 
 
 console.log("Hola");
 
 console.error("hay un error");
+
+//Tipos de datos en js
+
+// Cadenas
+
+"Hola mundo" //String
+
+'Hola mundo' // String
+
+//númerico
+1000
+-2.34
+
+//Boolean
+true  // Verdadero
+false // Falso
+
+// Array
+
+["Victor","Juan","Jairo"];
+[1,2,3];
+[true,false,true,false];
