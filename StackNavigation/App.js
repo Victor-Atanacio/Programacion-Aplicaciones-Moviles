90085bb86640680a46c179964b69eccb721dd7de
Victor-Atanacio <124050394@upq.edu.mx>
2025-11-18 16:19:47 -0600

    Navegation: Stack: completada

---
+++ StackNavigation/App.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import Setting from './screens/Setting';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Setting" component={Setting} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
